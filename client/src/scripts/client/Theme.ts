fffeb53383e0d3ebb648e676ec2da62004dc1e03
indeedornot <gravitybartek@gmail.com>
2023-02-13 22:31:14 +0100

    ref(client): Remove setDefaultTheme, due to being handled by context

--- client/src/scripts/client/Theme.ts
+++
@@ -1,5 +0,0 @@
-export const setDefaultTheme = () => {
-	const darkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
-	if (!darkMode) return;
-	document.body.classList.add('dark');
-};
